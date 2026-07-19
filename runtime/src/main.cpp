9063837de21b76f854abc719583acef587e74986
MacroStardust <gaming@cybertwip.com>
2026-07-18 20:22:05 -0600

    Fix Windows runtime

--- runtime/src/main.cpp
+++ runtime/src/main.cpp
@@ -3425,8 +3425,8 @@ static void sdl_vblank_present(void) {
 
 int main(int argc, char** argv) {
     /* Force line-buffered output so messages appear even if killed. */
-    std::setvbuf(stdout, nullptr, _IOLBF, 0);
-    std::setvbuf(stderr, nullptr, _IOLBF, 0);
+    std::setvbuf(stdout, nullptr, _IOLBF, BUFSIZ);
+    std::setvbuf(stderr, nullptr, _IOLBF, BUFSIZ);
     std::fprintf(stderr, "psxrecomp: main() entered\n");
     std::fflush(stderr);
 
